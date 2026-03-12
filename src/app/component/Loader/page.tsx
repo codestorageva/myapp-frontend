869f1e3ed7668df8bd06661f2228cda16ea29d09
codestorageva <codestorage.vaistra@gmail.com>
2026-03-12 13:15:52 +0530

    Fix: Add use client to Loader component

--- src/app/component/Loader/page.tsx
+++ src/app/component/Loader/page.tsx
@@ -1,35 +1,4 @@
-import React from 'react'
+'use client';
+'use client';
+'use client';
 
-import dynamic from 'next/dynamic';
-
-const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
-// import Loading from '../../../../public/assets/images/loader/Animation - 1745566403338.json'
-import Loading from '../../../../public/assets/images/loader/loading.json'
-import loaderStyle from "./loader.module.scss";
-
-const Loader = ({ isInside }: { isInside?: boolean }) => {
-    return (
-        <>
-            {isInside ? (
-                <div className='d-flex justify-content-center align-items-center'
-                    style={{ scale: 0.3 }}
-                >
-                    <Lottie
-                        loop
-                        animationData={Loading}
-                        play
-                        className='flex justify-center item-center'
-                    />
-                </div>
-            ) : (
-                <div className="fixed inset-0 flex justify-center items-center bg-white/50 z-50">
-                    <div className='justify-conten-center align-items-center' style={{ scale: 0.3 }}>
-                        <Lottie loop animationData={Loading} play />
-                    </div>
-                </div>
-            )}
-        </>
-    )
-}
-
-export default Loader
