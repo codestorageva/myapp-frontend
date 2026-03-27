7e06c35f441f77efee407cee51e5328284c754a5
Vaistra Codestorage <codestorage.vaistra@gmail.com>
2026-03-27 17:03:50 +0530

    Update page.tsx

--- src/app/component/Loader/page.tsx
+++ src/app/component/Loader/page.tsx
@@ -1,18 +1,36 @@
-'use client';
-import styles from './loader.module.scss';
+'use client'
+import React from 'react'
 
-export default function Loader() {
-  return (
-    <div className={styles.body}>
-      <div className={styles.truck}>
-        <div className={styles.base}></div>
-        <div className={styles.container}></div>
-        <div className={styles.cabin}>
-          <div className={styles.handle}></div>
-        </div>
-        <div className={styles.wheel1}></div>
-        <div className={styles.wheel2}></div>
-      </div>
-    </div>
-  );
+import dynamic from 'next/dynamic';
+
+const Lottie = dynamic(() => import('react-lottie-player'),{ ssr: false });
+// import Loading from '../../../../public/assets/images/loader/Animation - 1745566403338.json'
+import Loading from '../../../../public/assets/images/loader/loading.json'
+;
+
+const Loader = ({ isInside }: { isInside?: boolean }) => {
+    return (
+        <>
+            {isInside ? (
+                <div className='d-flex justify-content-center align-items-center'
+                    style={{ scale: 0.3 }}
+                >
+                    <Lottie
+                        loop
+                        animationData={Loading}
+                        play
+                        className='flex justify-center item-center'
+                    />
+                </div>
+            ) : (
+                <div className="fixed inset-0 flex justify-center items-center bg-white/50 z-50">
+                    <div className='justify-conten-center align-items-center' style={{ scale: 0.3 }}>
+                        <Lottie loop animationData={Loading} play />
+                    </div>
+                </div>
+            )}
+        </>
+    )
 }
+
+export default Loader
